5cb1524a67f37c1e7df319aea384f26238d149b6
Damien Letournel <damien.letournel@gmail.com>
2023-10-29 17:05:13 +0000

    save it please

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # pylint: disable=unused-argument
 # This program is dedicated to the public domain under the CC0 license.
-"""coincoin"""
+"""coincoin
+
+DATA MODEL
+
+Payload := (request_type, request_data)
+
+request_type := CB_REG|CB_SUG
+
+before
+ bot_data := {int_chat_id: int_id}
+after
+ bot_data := {int_id: int_chat_id}
+
+/!\ non persistent
+options := {int_id: string_household_name}
+
+
+"""
 
 
 import logging
@@ -31,6 +48,8 @@ CB_REG=0  # register a new peer Noel
 CB_SUG=1  # suggest a gift to a peer
 
 
+
+
 options = dict(zip(
     random.sample(list(range(6)), 6),
     ["Parents", "Anais & Lucas", "Geoffrey & Julie",
@@ -57,7 +76,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Choisissez votre pair Noel:", reply_markup=reply_markup)
 
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def suggest(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a message with three inline buttons attached."""
+
+    payload = {
+    }
+    context.bot_data.update(payload)
+
+    await update.message.reply_text("Choisissez pour qui vous voulez suggerer:",
+                                    reply_markup=reply_markup)
+
+
+async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Parses the CallbackQuery and updates the message text."""
     query = update.callback_query
 
@@ -66,9 +96,9 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await query.answer()
 
     data = context.bot_data
-    resp = int(query.data)
+    household_id = int(query.data[1])
 
-    if resp in context.bot_data.values():
+    if household_id in context.bot_data.values():
         await query.edit_message_text(text="Quelqu'un a déjà selectionné ces personnes",
                                       reply_markup=reply_markup)
         logger.error("au voleur !: %s", len(context.bot_data.values()))
@@ -79,10 +109,10 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                 )
         else:
             await query.edit_message_text(
-                text=f"J'ai bien noté que votre cadeau sera pour {options.get(resp)}")
+                text=f"J'ai bien noté que votre cadeau sera pour {options.get(household_id)}")
 
         payload = {
-            update.effective_chat.id: int(query.data)
+            update.effective_chat.id: household_id
         }
         context.bot_data.update(payload)
 
@@ -98,11 +128,58 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await context.bot.send_message(925808534, f"tout le monde a vote il reste {whosleft}")
 
 
+async def callback_register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
+
+    data = context.bot_data
+    household_id = int(query.data)
+
+    if household_id in context.bot_data.values():
+        await query.edit_message_text(text="Quelqu'un a déjà selectionné ces personnes",
+                                      reply_markup=reply_markup)
+        logger.error("au voleur !: %s", len(context.bot_data.values()))
+    else:
+        if update.effective_chat.id in data.keys():
+            await query.edit_message_text(
+                text="Vous aviez déjà choisi votre pair. Votre pair a été mis à jour."
+                )
+        else:
+            await query.edit_message_text(
+                text=f"J'ai bien noté que votre cadeau sera pour {options.get(household_id)}")
+
+        payload = {
+            household_id: update.effective_chat.id
+        }
+        context.bot_data.update(payload)
+
+
+    logger.info("new vote from %s count: %s",
+                query.from_user.first_name, len(context.bot_data.keys()))
+
+
+async def callback_suggest(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
+
+    data = context.bot_data
+    household_id = int(query.data)
+
+
+
 
 
 async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Display a help message"""
-    await update.message.reply_text("Use /quiz, /poll or /preview to test this bot.")
+    await update.message.reply_text("Use /start, /suggest or /help to test this bot.")
 
 
 def main() -> None:
@@ -113,7 +190,12 @@ def main() -> None:
         persistence(persistence=persistence).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CallbackQueryHandler(
+        callback=callback_register,
+        pattern=lambda d: d[0] == CB_REG))
+    application.add_handler(CallbackQueryHandler(
+        callback=callback_suggest,
+        pattern=lambda d: d[0] == CB_SUG))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
